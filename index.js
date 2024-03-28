2a836d3e7d0f0890298ca2afb6825dfdb91d88c2
prince mumo <princeinkunzi@gmail.com>
2024-03-28 16:51:21 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+// index.js
+
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+
